bf621784d1647c24d2c39c3af96ec05b5766cb19
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-15 22:47:36 +0200

    Remove venv directory from tracking and update .gitignore

--- flask/data_aug.py
+++ flask/data_aug.py
@@ -8,3 +8,6 @@ def generate_best_sold_period():
     start_month = random.randint(1, 12)
     end_month = random.randint(start_month, 12)
     return f"{start_month}-2024 to {end_month}-2024"
+
+input_file = 'data.xls'  
+df = pd.read_csv(input_file)
