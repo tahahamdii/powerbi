4d091425687fe0f1be417ae9bc82d5d745066b35
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-09-15 22:42:14 +0200

    best sold period

--- flask/data_aug.py
+++ flask/data_aug.py
@@ -3,3 +3,8 @@ import random
 
 def generate_units_sold():
     return random.randint(50, 500)
+
+def generate_best_sold_period():
+    start_month = random.randint(1, 12)
+    end_month = random.randint(start_month, 12)
+    return f"{start_month}-2024 to {end_month}-2024"
